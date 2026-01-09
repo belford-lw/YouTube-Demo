a55f63b67800e6f495c9479529e1b6708c49f155
belford-lw <sardoor.dev@gmail.com>
2026-01-09 22:38:50 +0500

    update web-site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
     <div>
-      <Footer/>
+        {/* HECH NARSA YOZILMAGAN */}
     </div>
   )
 }
